23da18a1990864c9e374ea212003b7b14332ce96
Arbaz3008 <114429714+Arbaz3008@users.noreply.github.com>
2025-01-20 15:16:06 +0500

    completed

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa6';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-const Checkout = () => {
+const Checkout = ({setOrder}) => {
   const [billingToggle, setBillingToggle] = useState(true);
   const [shippingToggle, setShippingToggle] = useState(false);
   const [paymentToggle, setPaymentToggle] = useState(false);
   const [paymentMethod, setPaymentMethod] = useState("COD");
+  const [shippingInfo , setShippingInfo] = useState({
+    address:'',
+    city:"",
+    zipcode:""
+  })
   const cart= useSelector(state=>state.cart)
+  const navigate = useNavigate()
+  const handleOrder = () => {
+    const newOrder = {
+      products: cart.products,
+      orderNumber:"1234567",
+      shippingInformation: shippingInfo,
+      totalPrice:cart.totalPrice
+    }
+    setOrder(newOrder)
+    navigate('/order')
+  }
 
   return (
     <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
@@ -73,6 +90,7 @@ const Checkout = () => {
                   name="address"
                   placeholder="Enter Address"
                   className="w-full px-3 py-2 border rounded-md"
+                  onChange={(e)=>setShippingInfo({...shippingInfo, address: e.target.value})}
                 />
               </div>
               <div>
@@ -82,6 +100,7 @@ const Checkout = () => {
                   name="city"
                   placeholder="Enter City Name"
                   className="w-full px-3 py-2 border rounded-md"
+                  onChange={(e)=>setShippingInfo({...shippingInfo, city: e.target.value})}
                 />
               </div>
               <div>
@@ -91,6 +110,7 @@ const Checkout = () => {
                   name="zipcode"
                   placeholder="Enter Zip Code"
                   className="w-full px-3 py-2 border rounded-md"
+                  onChange={(e)=>setShippingInfo({...shippingInfo, zipcode: e.target.value})}
                 />
               </div>
             </div>
@@ -173,28 +193,36 @@ const Checkout = () => {
         {/* Right Section */}
         <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border">
           <h3 className="text-lg font-semibold mb-4">Order Summary</h3>
-           <div>
+           <div className='space-y-4'>
              {cart.products.map(product=>(
-                <div>
-                    <div>
-                        <img src={product.image} alt=''/>
-                        <div>
-                            <h4>{product.name}</h4>
-                            <p>
+                <div key={product.id} className='flex justify-between'>
+                    <div className='flex items-center'>
+                        <img src={product.image} alt={product.name}
+                        className='w-16 h-16 object-contain rounded'
+                        />
+                        <div className='ml-4'>
+                            <h4 className='text-md font-semibold'>{product.name}</h4>
+                            <p className='text-gray-600'>
                                 &{product.orice} x {product.quantity}
                             </p>
                         </div>
                     </div>
+                    <div className='text-gray-800'>
+                      ${product.price}x{product.quantity}
+                    </div>
                 </div>
              ))}
            </div>
-           <div>
-            <div>
+           <div className='mt-4 border-t pt-4'>
+            <div className='flex justify-between'>
                 <span>Total Price :</span>
-                <span>${cart.totalPrice.toFixed(2)}</span>
+                <span className='font-semibold'>${cart.totalPrice.toFixed(2)}</span>
             </div>
            </div>
-           <button>Place Order</button>
+           <button
+           className='w-full bg-red-600 text-white py-2 mt-6 hover:bg-red-800'
+           onClick={handleOrder}
+           >Place Order</button>
         </div>
       </div>
     </div>
